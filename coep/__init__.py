639cce3ece42d4eed3fdca7ebb33151fed7b62c7
kasmith <k2smith@mit.edu>
2018-10-15 09:57:10 -0400

    Exposing DaskManager

--- coep/__init__.py
+++ coep/__init__.py
@@ -3,11 +3,12 @@ Loads in all relevant functions, but __all__ only exposes the optimizer and
 required functions
 """
 
-from .managers import Producer, ProducerManager
+from .managers import Producer, ProducerManager, DaskManager
 from .optimizers import SPSA
 from .objective_funcs import ObjectiveProcessor, default_objective
 from .coep import COEP
 from .database import write_function_aux
 
 
-__all__ = ['SPSA', 'ProducerManager', 'ObjectiveProcessor', 'COEP']
+__all__ = ['SPSA', 'ProducerManager', 'ObjectiveProcessor', 'COEP',
+           'DaskManager']
