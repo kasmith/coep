6a9bb4c775c530607239d19e61e9cf37c64955d7
kasmith <k2smith@mit.edu>
2018-10-22 11:57:14 -0400

    Added single threading

--- coep/__init__.py
+++ coep/__init__.py
@@ -3,7 +3,7 @@ Loads in all relevant functions, but __all__ only exposes the optimizer and
 required functions
 """
 
-from .managers import Producer, ProducerManager, DaskManager
+from .managers import Producer, ProducerManager, DaskManager, SingleThreadManager
 from .optimizers import SPSA, grid_search
 from .objective_funcs import ObjectiveProcessor, default_objective
 from .coep import COEP
@@ -11,4 +11,4 @@ from .database import write_function_aux
 
 
 __all__ = ['SPSA', 'ProducerManager', 'ObjectiveProcessor', 'COEP',
-           'DaskManager', 'grid_search']
+           'DaskManager', 'grid_search', 'SingleThreadManager']
--- coep/managers/__init__.py
+++ coep/managers/__init__.py
@@ -5,5 +5,6 @@ Stores all of the ProcManagers for parallelization
 from .core import _empty_func, ProcManager
 from .producer import Producer, ProducerManager
 from .dask import DaskManager
+from .singlethread import SingleThreadManager
 
-__all__ = ['ProcManager', 'ProducerManager', 'Producer', 'DaskManager']
+__all__ = ['ProcManager', 'ProducerManager', 'Producer', 'DaskManager', 'SingleThreadManager']
